dd9b40445c404407e42b02f38bbc0aa98d33f49f
Victor Lozano <elviclozano@gmail.com>
2021-05-01 19:22:43 -0500

    primero commit

---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-contador',
+    template: `
+        
+        <!-- <h1>{{titulo}}</h1>
+        <h2>Hola Mundo!!!</h2>
+        <h3>La base es <strong>{{base}}</strong></h3> -->
+
+
+        <button (click)="operacion(+base);"> + {{ base }}</button>
+        <span>{{numero}}</span>
+        <button (click)="operacion(-base);"> - {{base}}</button>
+
+    `
+})
+export class ContadorComponet {
+    titulo:string = 'Contador App';
+    numero:number=10;
+    base:number=5;
+  
+    operacion(valor:number){
+      this.numero+=valor;
+    }
+}
